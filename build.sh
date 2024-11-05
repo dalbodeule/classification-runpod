d8571c1af0f0e01905572ab6ebb2d251d400b96f
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-11-05 12:20:44 +0900

    update 17

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ fi
 DOCKER_BUILDKIT=1 docker buildx build --file Dockerfile --platform linux/amd64,linux/arm64 -t ${IMAGE_NAME}:cpu-${VERSION_TAG} -t ${IMAGE_NAME}:cpu-${DATE_TAG} -t ${IMAGE_NAME}:cpu . --push
 
 # Build Dockerfile.cuda (CUDA version)
-DOCKER_BUILDKIT=1 docker buildx build --file Dockerfile.cuda --platform linux/amd64,linux/arm64 -t ${IMAGE_NAME}:cuda-${VERSION_TAG} -t ${IMAGE_NAME}:cuda-${DATE_TAG} -t ${IMAGE_NAME}:cuda . --push
+DOCKER_BUILDKIT=1 docker buildx build --file Dockerfile.cuda --platform linux/amd64 -t ${IMAGE_NAME}:cuda-${VERSION_TAG} -t ${IMAGE_NAME}:cuda-${DATE_TAG} -t ${IMAGE_NAME}:cuda . --push
 
 echo "Docker images for amd64 and arm64 (CPU and CUDA versions) have been built and pushed successfully."
 
